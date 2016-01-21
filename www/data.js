cd93750760c981188e122a1c802f0f2b22737e41
Julio Cesar Fausto <jcfausto@gmail.com>
2016-01-21 17:23:19 -0200

    Creating the dashboard

---
+++ www/data.js
@@ -0,0 +1,105 @@
+var results = {};
+
+results["20161"] =
+
+    [
+        {
+            "team": "França",
+            "process": 3,
+            "codequality": 6,
+            "errornumber": 4,
+            "productivity": 13
+        },
+        {
+            "team": "GeInfo",
+            "process": 8,
+            "codequality": 7,
+            "errornumber": 7,
+            "productivity": 22
+        },
+        {
+            "team": "Águia",
+            "process": 10,
+            "codequality": 11,
+            "errornumber": 5,
+            "productivity": 26
+        }
+    ];
+
+results["20162"] =
+
+    [
+        {
+            "team": "França",
+            "process": 10,
+            "codequality": 13,
+            "errornumber": 8,
+            "productivity": 7
+        },
+        {
+            "team": "GeInfo",
+            "process": 18,
+            "codequality": 21,
+            "errornumber": 14,
+            "productivity": 12
+        },
+        {
+            "team": "Águia",
+            "process": 13,
+            "codequality": 18,
+            "errornumber": 10,
+            "productivity": 33
+        }
+    ];    
+
+results["20163"] =
+
+    [
+        {
+            "team": "França",
+            "process": 33,
+            "codequality": 66,
+            "errornumber": 43,
+            "productivity": 19
+        },
+        {
+            "team": "GeInfo",
+            "process": 80,
+            "codequality": 77,
+            "errornumber": 77,
+            "productivity": 22
+        },
+        {
+            "team": "Águia",
+            "process": 100,
+            "codequality": 14,
+            "errornumber": 59,
+            "productivity": 68
+        }
+    ];
+
+results["20164"] =
+
+    [
+        {
+            "team": "França",
+            "process": 33,
+            "codequality": 66,
+            "errornumber": 44,
+            "productivity": 13
+        },
+        {
+            "team": "GeInfo",
+            "process": 86,
+            "codequality": 78,
+            "errornumber": 72,
+            "productivity": 42
+        },
+        {
+            "team": "Águia",
+            "process": 12,
+            "codequality": 13,
+            "errornumber": 55,
+            "productivity": 26
+        }
+    ];    
